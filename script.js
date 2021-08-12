6460eab875b839a4b2cca5a2725f54c366c6ea90
Mayckon Rebecci <mayckon_mmr@hotmail.com>
2021-08-12 00:05:26 -0300

    adicionado programa full funcional

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
 let body = document.getElementById("gradient");
 
+function bodyStyleOnInit () {
+    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+    
+    cssBgText.innerHTML = body.style.background + ";";
+}
+
 function setGradient() {
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
     
@@ -12,7 +18,4 @@ function setGradient() {
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
 
-function bodyStyleOnInit () {
-    cssBgText.innerHTML =  "linear-gradient(to right, " + color1.value + ", " + color2.value + ");";
-}
 
