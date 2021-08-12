6ee10f6ce139b9319876d5338a1d787a651426a4
Mayckon Rebecci <mayckon_mmr@hotmail.com>
2021-08-11 23:48:05 -0300

    adicinado programa full funcional

---
+++ readme.md
@@ -0,0 +1,8 @@
+## Gradient Color Generator
+
+Gerador de cor gradiente(degradê) feito com HTML, CSS e Javascript.
+
+Gradient color generator with HTML, CSS and Javascript.
+
+<img src="/img/screenshot-2.png">
+<img src="/img/screenshot-1.png">
---
+++ script.js
@@ -0,0 +1,23 @@
+let cssBgText = document.querySelector(".css-bg-text");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
+
+function setGradient() {
+    body.style.background = 
+    "linear-gradient(to right, " 
+    + color1.value
+    + ", " 
+    + color2.value 
+    + ")";
+    
+    cssBgText.innerHTML = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+
+function bodyStyleOnInit () {
+    cssBgText.innerHTML =  "linear-gradient(to right, " + color1.value + ", " + color2.value + ");";
+}
+
